33762e2dfdd616c4cd749c67a61d4587974d41b1
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-10-29 20:58:22 +0300

    added

---
+++ script.js
@@ -0,0 +1,106 @@
+const api = {
+    endpoint: "https://api.openweathermap.org/data/2.5/",
+    key: "2fee88555cf69ae72c559fd0b521e98a"
+ }
+ 
+ const input = document.querySelector("#input");
+ input.addEventListener("keypress", enter);
+ 
+ function enter(e) {
+    if (e.keyCode === 13) {
+       getInfo(input.value);
+    }
+ }
+ 
+ async function getInfo(data) {
+    const res = await fetch(`${api.endpoint}weather?q=${data}&units=metric&appID=${api.key}`);
+    const result = await res.json();
+    displayResult(result);
+ }
+ 
+ function displayResult(result) {
+    let city = document.querySelector("#city");
+    city.textContent = `${result.name}, ${result.sys.country}`;
+ 
+    getOurDate();
+ 
+    let temperature = document.querySelector("#temperature");
+    temperature.innerHTML = `${Math.round(result.main.temp)}<span>°</span>`;
+ 
+    let feelsLike = document.querySelector("#feelsLike");
+    feelsLike.innerHTML = "Feels like: " + `${Math.round(result.main.feels_like)}<span>°</span>`;
+ 
+    let conditions = document.querySelector("#conditions");
+    conditions.textContent = `${result.weather[0].main}`;
+ 
+    let variation = document.querySelector("#variation");
+    variation.innerHTML = "Min: " + `${Math.round(result.main.temp_min)}<span>°</span>` + " " + "Max " + `${Math.round(result.main.temp_max)}<span>°</span>`
+ 
+ }
+ 
+ function getOurDate() {
+    const myDate = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+ 
+    let day = days[myDate.getDay()];
+ 
+    let todayDate = myDate.getDate();
+ 
+    let month = months[myDate.getMonth()];
+ 
+    let year = myDate.getFullYear();
+ 
+    let showDate = document.querySelector("#date");
+    showDate.textContent = `${day}` + " " + `${todayDate}` + " " + `${month}` + " " + `${year}`
+ }
+ 
+ gsap.from("#header", {
+    y: -300,
+    delay: 0.2,
+    duration: 3,
+    opacity: 0.5,
+    ease: "power4.out"
+ })
+ gsap.from("#city", {
+    y: -300,
+    delay: 1.2,
+    duration: 3,
+    opacity: 0.5,
+    ease: "power4.out"
+ })
+ gsap.from("#date", {
+    y: -300,
+    delay: 2.2,
+    duration: 3,
+    opacity: 0,
+    ease: "power4.out"
+ })
+ gsap.from("#temperature", {
+    y: -300,
+    delay: 3.2,
+    duration: 3,
+    opacity: 0,
+    ease: "power4.out"
+ })
+ gsap.from("#feelsLike", {
+    y: -300,
+    delay: 4.2,
+    duration: 3,
+    opacity: 0,
+    ease: "power4.out"
+ })
+ gsap.from("#conditions", {
+    y: -300,
+    delay: 5.2,
+    duration: 3,
+    opacity: 0,
+    ease: "power4.out"
+ })
+ gsap.from("#variation", {
+    y: 300,
+    delay: 6.2,
+    duration: 3,
+    opacity: 0,
+    ease: "power4.out"
+ })
